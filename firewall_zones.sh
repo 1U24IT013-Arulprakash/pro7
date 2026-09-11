0769ba2c10dfbfec32e77b3b7b75ac2d7baba4ec
1U24IT013-Arulprakash <marulprakash35@gmail.com>
2026-09-11 12:23:35 +0530

    Update firewall_zones.sh

--- firewall_zones.sh
+++ firewall_zones.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
 
-# Student Name:arul prakash m 
-# Register Number:1U24IT013
+# Student Name: arul prakash
+# Register Number: 1U24IT013
 
 # 1. Display all available firewall zones
+# TODO
 firewall-cmd --get-zones
 
 # 2. Display current default zone
+# TODO
 firewall-cmd --get-default-zone
 
 # 3. Display information about public zone
+# TODO
 firewall-cmd --zone=public --list-all
 
 # 4. Set default zone to internal
+# TODO
 firewall-cmd --set-default-zone=internal
 
 # 5. Verify the new default zone
+# TODO
 firewall-cmd --get-default-zone
-
-exit 0#!/bin/bash
-
-# Student Name:
-# Register Number:
-
-# 1. Display all available firewall zones
-# TODO
-
-# 2. Display current default zone
-# TODO
-
-
-# 3. Display information about public zone
-# TODO
-
-# 4. Set default zone to internal
-# TODO
-
-
-# 5. Verify the new default zone
-# TODO
-
 
 
 
